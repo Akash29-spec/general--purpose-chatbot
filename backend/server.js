02c7611e3282f8c7d31c4a8c8b640067125e0767
Akash29-spec <sakhareakash404@gmail.com>
2026-05-09 13:30:24 +0530

    crated user message feature

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+const OLLAMA_API = 'http://localhost:11434/api/generate';
+
+// API endpoint for chat
+app.post('/api/chat', async (req, res) => {
+    console.log(req.body);
+    const { message, model } = req.body;
+
+    
+    if (!message) {
+        return res.status(400).json({ error: 'Message required' });
+    }
+    
+    try {
+        const response = await axios.post(OLLAMA_API, {
+            model: model || 'codellama',
+            prompt: message,
+            stream: false
+        });
+        res.json({ reply: response.data.response });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+app.listen(5000, () => {
+    console.log('Server running on http://localhost:5000');
+});
+// Server listens on port 5000
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-function sendmessage(){
+function sendMessage(){
     const userInput = document.getElementById("userInput");
-    
+    godown()
     createchatmessage(userInput.value,"user");
     userInput.value="";
 }
 
-function createchatmessage(message,sendre){
+function createchatmessage(message,sender){
     const chatmassage =document.createElement("div");
     const senderimg =document.createElement("img");
     const para = document.createElement("p");
-    senderimg.src = sendre === "user" ? "src/user.png" : "src/logo.png";
-    senderimg.classList.add("logo")
+    senderimg.src = sender === "user" ? "src/user.png" : "src/logo.png";
+    chatmassage.classList.add(sender);
+    senderimg.classList.add('logo')
     chatmassage.classList.add("chatmessage");
     para.classList.add("message");
     para.innerText=message;
@@ -18,5 +19,21 @@ function createchatmessage(message,sendre){
     chatmassage.appendChild(senderimg);
     const container =document.querySelector(".chatMessage");
     container.appendChild(chatmassage);
+    chatBotResponse(message);
+}
+function chatBotResponse(message){
+    fetch("http://localhost:5000/api/chat")
+    
+    .then(response => response.json())
+    .then(data => {
+        createchatmessage(data.reply, "bot");
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
 }
 
+function godown(){
+    const container = document.querySelector(".chatMessage");
+    container.scrollTop = container.scrollHeight;
+}
